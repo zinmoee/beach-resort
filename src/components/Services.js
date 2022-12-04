3d798dc7d23b9e098149f12fcc4e5e1422d8bb28
Zin Moe <dev.zinmoe@gmail.com>
2022-12-04 23:15:55 +0630

    assets & components

---
+++ src/components/Services.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa'
+import Title from './Title'
+
+export default class Services extends Component {
+    state = {
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: 'Free cocktails',
+                info: 'Lorem ipsum dolor sit met consectetur'
+            },
+            {
+                icon: <FaHiking />,
+                title: 'Endless Hiking',
+                info: 'Lorem ipsum dolor sit met consectetur'
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: 'Free shuttle',
+                info: 'Lorem ipsum dolor sit met consectetur'
+            },
+            {
+                icon: <FaBeer />,
+                title: 'Free Beer',
+                info: 'Lorem ipsum dolor sit met consectetur'
+            },
+        ]
+    }
+    render() {
+        return (
+            <section className='services'>
+                <Title title='services' />
+                <div className='services-center'>
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className='service'>
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+                </div>
+            </section>
+        )
+    }
+}
